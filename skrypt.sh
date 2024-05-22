bcce8867d27ad34d3e1c86d26263a76c3a2d0d9b
ViltisHere <149478121+ViltisHere@users.noreply.github.com>
2024-05-22 12:36:03 +0200

    utworzono --logs

--- skrypt.sh
+++ skrypt.sh
@@ -7,9 +7,21 @@ menu_options() {
 what_date() {
     date
 }
+log_files() {
+    local files=${1:-100}
+    for i in $(seq 1 $files); do
+        filename="log${i}.txt"
+	echo "Plik: $filename" > $filename
+	echo "Skrypt: $0" >> $filename
+	echo "Data: $(date)" >> $filename
+    done
+}
 
 case "$1" in
     --date)
         what_date
+        ;;
+    --logs)
+        log_files ${2:-100}
         ;;
 esac
