047fa958930c6bea46faeed8908a70d0ce3f6e67
ViltisHere <149478121+ViltisHere@users.noreply.github.com>
2024-05-22 13:20:06 +0200

    dodanie flagi --error

--- skrypt.sh
+++ skrypt.sh
@@ -2,8 +2,10 @@
 menu_options() {
     echo "Wybierz parametr:"
     echo "--date/-d Pokazuje date."
-    echo "--logs/-l Tworzy podana ilosc plikow."
+    echo "--logs/-l Tworzy podana ilosc plikow log."
     echo "--help/-h Wyswietli liste parametrow."
+    echo "--init/-i Sklonuje repozytorium."
+    echo "--error/-e Tworzenie podana ilosc plików error."
 }
 
 what_date() {
@@ -20,6 +22,21 @@ log_files() {
     done
 }
 
+error_files() {
+    local files=${1:-100}
+    mkdir -p error
+    for i in $(seq 1 $files); do
+        filename="error/error${i}.txt"
+	echo "Plik: $filename" > $filenama
+        echo "Skrypt: $0" >> $filename
+        echo "Data: $(date)" >> $filename
+    done
+}
+
+clone_repository() {
+    git clone https://github.com/ViltisHere/Narzedzia_IT_Lab4.git
+    export PATH=$(pwd):$PATH
+}
 case "$1" in
     --date|-d)
         what_date
@@ -29,5 +46,11 @@ case "$1" in
         ;;
     --help|-h)
         menu_options
+        ;;
+    --init|-i)
+        clone_repository
+        ;;
+    --error|-e)
+        error_files
         ;;
 esac
